371234178aba1496498393bbc2ee69e90474b299
samrose <samuel.rose@gmail.com>
2019-02-22 17:22:05 -0500

    update to show rustc version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    const MANIFEST: &'static str = env!("CARGO_MANIFEST_DIR");
-    println!("Manifest Dir {}", MANIFEST);
+    const MANIFEST: &'static str = env!("RUSTC --version");
+    println!("Rust version {}", MANIFEST);
 }
